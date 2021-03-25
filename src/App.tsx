fd9989b5a8e69a1ed2ff3b52c36a2f3a3322c5f5
Gustavo <kyoro_souza@hotmail.com>
2021-03-24 21:33:10 -0300

    ✨ Header add

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import {GlobalStyle} from './styles/global'
+import { Header } from './components/Header';
 
 function App() {
   return (
-    <div>
-
+    <>
+      <Header />
       <GlobalStyle />
-      <h1>Hello galera</h1>
-    </div>
+    </>
   );
 }
 
