2ede6883e9fd2f30470680a8bede83e9334b140c
Gustavo <kyoro_souza@hotmail.com>
2021-04-12 23:00:49 -0300

    🎨 create context for Transactions

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import {GlobalStyle} from './styles/global'
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
 import { NewTransactionModal } from './components/NewTransactionModal';
-import { TransactionsContext } from './TransactionsContext';
+import { TransactionsProvider } from './TransactionsContext';
 import Modal from 'react-modal';
 import { useState } from 'react';
 
@@ -21,9 +21,7 @@ function App() {
   }
 
   return (
-    <TransactionsContext.Provider
-      value={[]}
-    >
+    <TransactionsProvider>
       <Header 
         onOpenNewTransactionModal={handleOpenNewTransactionModal} 
       />
@@ -35,7 +33,7 @@ function App() {
       />
 
       <GlobalStyle />
-    </TransactionsContext.Provider>
+    </TransactionsProvider>
   );
 }
 
